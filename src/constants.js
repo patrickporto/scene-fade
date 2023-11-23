7360dadc3f77976d07501cf3082d6a045a234e69
Patrick Porto <patrick.s.porto@gmail.com>
2023-11-22 18:57:39 -0700

    feat: add scene fade

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const CANONICAL_NAME = 'fvtt-module-template';
-export const MODULE_NAME = 'FVTT Module Template';
+export const CANONICAL_NAME = 'scene-fade';
+export const MODULE_NAME = 'Scene Fade';
 export const MODULE_PATH = `modules/${CANONICAL_NAME}`;
 export const TEMPLATE_PATH = `${MODULE_PATH}/templates`;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,27 @@
 import { MODULE_NAME } from "./constants.js";
+import "./scene-fade.css"
 
-Hooks.on('init', async () => {
+let board = null
+const boardTransitionBackground = $(`<div class="scene-fade__background"></div>`)
+
+Hooks.on('canvasInit', async () => {
     console.log(`${MODULE_NAME} | Initializing ${MODULE_NAME}`);
+    board = document.getElementById("board");
+    $(boardTransitionBackground).insertBefore(board)
+    $(board).addClass("scene-fade").addClass("scene-fade--initial");
+});
+
+Hooks.on('canvasTearDown', async (canvas) => {
+    console.log(`${MODULE_NAME} | Canvas is being torn down`);
+    // console.log(canvas.masks.canvas)
+    console.log(canvas)
+    $(board).addClass("scene-fade--transitioning");
+});
+
+Hooks.on("canvasReady", async () => {
+    console.log(`${MODULE_NAME} | Canvas is ready`);
+    // setTimeout(() => {
+    //     $(board).removeClass("scene-fade--transitioning");
+    // }, 10000);
+    $(board).removeClass("scene-fade--transitioning").removeClass("scene-fade--initial");
 });
